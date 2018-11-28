2a1bfefcb1c798feb9ac3b2dd90187d61ba43c70
olo2552 <a.wielgorski@protonmail.com>
2018-11-28 19:54:04 +0100

    Exercise 1.5

---
+++ exercise_1-5/exercise_1-5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+float fahrenheitToCelcius (float fahrenheit) {
+    return (fahrenheit + 32) * (5.0/9.0);
+}
+
+float celsiusToFahrenheit (float celsius) {
+    return (5.0/9.0) * (celsius - 32.0);
+}
+
+void displayTableRow (float celsius, float fahrenheit) {
+    printf("%3.0f %6.1f\n", fahrenheit, celsius);    
+}
+
+void displayTemperatureTable (int lowerRange, int higherRange, int step) {
+    for (int temperature = higherRange; temperature >= lowerRange; temperature -= step) {
+        displayTableRow(temperature, celsiusToFahrenheit(temperature));
+    }
+}
+
+int main() {
+    displayTemperatureTable(20, 100, 1);
+
+    return 0;
+}
