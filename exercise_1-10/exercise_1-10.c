bce76294cfe44481eab53d381682fa05bf2368ef
olo2552 <a.wielgorski@protonmail.com>
2018-11-29 18:59:04 +0100

    Exercise 1.10

---
+++ exercise_1-10/exercise_1-10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int inputChar;
+    while (inputChar != '\n') {
+        inputChar = getchar();
+
+        switch (inputChar) {
+            case '\t':
+                printf("\\t");
+                break;
+            case ' ':
+                printf(" ");
+                break;
+            case '\\':
+                printf("\\");
+                break;
+            case '\b':
+                printf("\\b");
+                break;
+            default:
+                printf("%c", inputChar);
+                break;
+        }
+    }
+    printf("\n");
+}
